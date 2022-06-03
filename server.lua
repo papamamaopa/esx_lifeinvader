add9a69a9c511c34532666bde8f6c3a54d3ee0c7
papamamaopa <steam.philb@gmail.com>
2022-06-03 17:03:45 +0200

    money minus fix 3

--- server.lua
+++ server.lua
@@ -6,17 +6,17 @@ local lastAdDate = 0
 
 RegisterServerEvent("SyncWerbung")
 AddEventHandler('SyncWerbung', function(inputText)
+	local _source = source
+	local xPlayerSendedBy = ESX.GetPlayerFromId(_source)
 	local xPlayer = ESX.GetPlayerFromId(source)
+	lastAdDate = os.time(os.date("!*t"))
 	if os.time(os.date("!*t")) > (lastAdDate + Config.Time) then
 		if xPlayer.getMoney() >= Config.price then
-			lastAdDate = os.time(os.date("!*t"))
-			local _source = source
-			local xPlayer = ESX.GetPlayerFromId(_source)
-			TriggerClientEvent('DisplayWerbung', -1, inputText, xPlayer.getName())
+			TriggerClientEvent('DisplayWerbung', -1, inputText, xPlayerSendedBy.getName())
 
 			xPlayer.removeMoney(Config.Price)
 		else
-			TriggerClientEvent('AdError', source, "Du hast nicht genug Geld dabei!")
+			TriggerClientEvent('AdError', source, "Du hast nicht genug Geld dabei!.")
 		end
 	else
 		TriggerClientEvent('AdError', source, "In den letzten 30 Minuten wurde bereits Werbung geschaltet.")
